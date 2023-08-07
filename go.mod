b8cf4502c1a0df4e3cdd6491ac12e6584a1440cd
SrimanNarayana Vema <srimannarayana.vema@broadcom.com>
2023-08-07 14:18:02 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/srimanster/hellogo
+
+go 1.20
---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+import "rsc.io/quote"
+import "github.com/srimanster/functions/funct1"
+
+func main() {
+   fmt.Println("Hello World...!!!")
+   fmt.Println(quote.Go())
+}
